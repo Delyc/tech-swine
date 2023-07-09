a4f8f7808a98116213257f3b18d75dbe897ba7ac
delyc <d.twizeyima@alustudent.com>
2023-07-09 11:03:32 +0200

    Feat: What we offer

--- components/sections/WhatWeOffer.tsx
+++ components/sections/WhatWeOffer.tsx
@@ -1,5 +1,7 @@
 import test from '../../public/assets/hydrop.jpg'
 import Image from 'next/image'
+import pig from '../../public/assets/pigg.png'
+import fly from '../../public/assets/fly.jpg'
 const WhatWeOffer = () => {
 
     const services = [
@@ -9,30 +11,28 @@ const WhatWeOffer = () => {
             desciption: "We provide high-quality pigfor sale, carefully bred and raised with utmost care."
         },
         {
-            icon: "/assets/hydrop.jpg",
-            service: "Hydroponic Fodder Production",
-            desciption: "Provide hydroponic systems and expertise to produce nutrient-rich fodder for pig farms."
-        },
-        {
-            icon: "/assets/fly.jpg",
-            service: "Black Soldier Fly Integration",
-            desciption: "Offer consultancy and support for integrating black soldier flies into pig farming operations."
-        },
-        {
             icon: "/assets/circular.png",
             service: "Circular Economy Solutions",
             desciption: "Assist pig farmers in adopting circular economy practices by helping them design and implement waste management systems. "
         },
-        {
-            icon: "/assets/susfarm.jpg",
-            service: "Sustainable Farming Consultancy",
-            desciption: "Provide expert advice and consultation on sustainable pig farming practices."
-        },
-        {
-            icon: "/assets/resdev.jpg",
-            service: "Research and Development",
-            desciption: "Engage in research and development activities to continuously improve and innovate within the field of sustainable pig farming."
-        },
+        // {
+        //     icon: "/assets/hydrop.jpg",
+        //     service: "Hydroponic Fodder Production",
+        //     desciption: "Provide hydroponic systems and expertise to produce nutrient-rich fodder for pig farms."
+        // },
+        // {
+        //     icon: "/assets/fly.jpg",
+        //     service: "Black Soldier Fly Integration",
+        //     desciption: "Offer consultancy and support for integrating black soldier flies into pig farming operations."
+        // },
+     
+    
+
+    ]
+
+
+    const services2 = [
+    
         {
             icon: "/assets/manure.png",
             service: "Manure selling",
@@ -43,15 +43,65 @@ const WhatWeOffer = () => {
             service: "Breeding",
             desciption: "Partner with us for professional pig breeding services. Ensuring proper mating and reproduction"
         },
+  
     ]
-    return ( 
+    return (
         <>
-
-        <div className='flex flex-col gap-4 items-center mt-32 mb-12'>         
-        <h2 className="text-primary-green font-bold text-2xl">What We Offer</h2>
+            <section className='bg-[#FFF9F9] px-40 py-10 mt-10'>
+                <div className='flex flex-col gap-4 items-center mb-12'>
+                    <h2 className="text-primary-green font-bold text-2xl">What We Offer</h2>
                     <p className="text-dark-blue">Experience excellence in pig farming with our comprehensive suite of services</p>
-            </div>
-        <section className='grid grid-cols-2 px-44 gap-20'>
+                </div>
+                <div className='grid grid-cols-4'>
+                    <div className='flex flex-col gap-5 '>
+                        {services.map((service, index) => {
+                            return (
+                                <div key={index} className=' flex flex-col gap-2   rounded p-5'>
+                                   {/* <div className='flex items-center'> */}
+                                    <Image src={service.icon} alt="test" width={100} height={100} className='rounded-full'/>
+                                    {/* <div> */}
+                                        <p className=' text-dark-blue w-4/5 '>{service.service}</p>
+                                      {/* </div> */}
+                                        <p className=' text-sm'>{service.desciption}</p>
+                                    {/* </div> */}
+                                </div>
+                            )
+                        }
+                        )}
+                    </div>
+                    <div className='col-span-2 flex flex-col gap-5 items-center justify-center'>
+                    <div className=' flex flex-col gap-2 items-start  p-5 rounded w-1/2'>
+                                   {/* <div className='flex items-center'> */}
+                                    <Image src={fly} alt="test" width={100} height={100} className='rounded-full'/>
+                                    {/* <div> */}
+                                        <p className='w-3/5 text-left text-dark-blue'>Black Soldier Fly Integration</p>
+                                      {/* </div> */}
+                                        <p className=' text-left text-sm'>Offer consultancy and support for integrating black soldier flies into pig farming operations.</p>
+                                    {/* </div> */}
+                                </div>
+                        {/* <Image src={pig} alt="test" width={100} height={100} className='w-3/5' /> */}
+      
+
+                    </div>
+                    <div className='flex flex-col gap-5'>
+                    {services2.map((service, index) => {
+                            return (
+                                <div key={index} className=' flex flex-col gap-2 items-start  p-5 rounded'>
+                                   {/* <div className='flex items-center'> */}
+                                    <Image src={service.icon} alt="test" width={100} height={100} className='rounded-full'/>
+                                    {/* <div> */}
+                                        <p className='w-3/5 text-left text-dark-blue'>{service.service}</p>
+                                      {/* </div> */}
+                                        <p className=' text-left text-sm'>{service.desciption}</p>
+                                    {/* </div> */}
+                                </div>
+                            )
+                        }
+                        )}
+
+                    </div>
+                </div>
+                {/* <section className='grid grid-cols-2 px-44 gap-20'>
 
             {services.map((service, index) => {
                 return(
@@ -67,9 +117,10 @@ const WhatWeOffer = () => {
             }
             )}
 
-        </section>
+        </section> */}
+            </section>
         </>
-     );
+    );
 }
- 
+
 export default WhatWeOffer;
